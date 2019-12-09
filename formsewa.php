1a5246665ac7f87750795062c7365a4baa6fae59
putribudiasih <putribudiasih18@gmail.com>
2019-12-09 15:40:31 +0700

    form sewa alat

---
+++ formsewa.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Form Sewa</title>
+</head>
+<body>
+	<h2>Form Sewa Alat Pertanian</h2>
+	<form action="proses.php" method="get">
+		Nama: <input type="text" name="nama" />
+		<br />
+		Alamat: <input type="text" name="alamat" />
+		<br />
+		Nomor Telepon: <input type="text" name="notelepon" />
+		<br />
+		Nomor KTP: <input type="text" name="noktp" />
+		<br />
+		Lama Sewa: <input type="text" name="lama" />
+		<br />
+		Asal Kota/Kab: <input type="text" name="asal" />
+		<br />
+		<input type="submit" value="Lanjut" />
+	</form>
+</body>
+</html>
